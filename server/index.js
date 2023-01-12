dd998d5737aedaa13a3eff6ff16bdbaf0399d46c
lisandroveron <lisandro.veron@hotmail.com>
2023-01-12 16:15:23 -0300

    Removed: Require for 'process' module.

--- server/index.js
+++ server/index.js
@@ -20,7 +20,6 @@ app.get("/getTiles", (req, res) => {
 	});
 });
 
-const process = require("process");
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
 	console.log(`Listening on port ${port}.`);
