acd21ff3b769c078f097d97714f82729e678476e
lisandroveron <lisandro.veron@hotmail.com>
2023-01-17 20:44:31 -0300

    Added: Basic login function.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,32 +1,52 @@
 import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+// Routes
 import Home from "./routes/Home/Home.jsx";
 import GuideMaker from "./routes/GuideMaker/GuideMaker.jsx";
+// Components
 
 function App(){
-	const [path_tiles, setPathTiles] = useState([]);
+	const [isLogged, setIsLogged] = useState(false);
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	// const [path_tiles, setPathTiles] = useState([]);
 
 	useEffect(() => {
-		getAssets("tiles", "*_0.jpg", setPathTiles);
-	}, []);
+		
+	}, [
+		// getAssets("tiles", "*_0.jpg", setPathTiles)
+	]);
 
 	// Functions
-	function getAssets(resource, pattern, updateFunction){
-		fetch("/getAssets", {
+	// function getAssets(resource, pattern, updateFunction){
+	// 	fetch("/getAssets", {
+	// 		method: "POST",
+	// 		headers: {'Content-Type': 'application/json'},
+	// 		body: JSON.stringify({
+	// 			"resource": resource,
+	// 			"pattern": pattern
+	// 		})
+	// 	})
+	// 		.then(response => response.json())
+	// 		.then(data => {
+	// 			for(let path of data){
+	// 				updateFunction(oldArray => [...oldArray, path]);
+	// 			};
+	// 		});
+	// };
+	function login(event){
+		event.preventDefault();
+		fetch("/login", {
 			method: "POST",
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
-				"resource": resource,
-				"pattern": pattern
+				username,
+				password,
 			})
 		})
 			.then(response => response.json())
-			.then(data => {
-				for(let path of data){
-					updateFunction(oldArray => [...oldArray, path]);
-				};
-			});
+			.then(body => setIsLogged(body.verified));
 	};
 	function removeParent(tag){
 		tag.target.parentElement.remove();
@@ -38,10 +58,18 @@ function App(){
 				<p>"Proyecto Wardear" no cuenta con el respaldo de Riot Games y no refleja los puntos de vista ni las opiniones de Riot Games ni de nadie involucrado oficialmente en la producción o administración de la propiedad de Riot Games. Riot Games y todas las propiedades asociadas son marcas comerciales o marcas comerciales registradas de Riot Games, Inc.</p>
 				<input type="button" value="X" onClick={removeParent} />
 			</div>
+			{isLogged
+			?<p>Hello, {username}</p>
+			:<form id="login" onSubmit={login}>
+				<input type="text" name="user" onChange={(event) => {setUsername(event.target.value)}} />
+				<input type="password" name="password" onChange={(event) => {setPassword(event.target.value)}} />
+				<input type="submit" value="Iniciar sesión" />
+			</form>
+			}
 			<h1>Proyecto Wardear</h1>
-			<div className="tiles">
+			{/*<div className="tiles">
 				{path_tiles.map((path, index) => <img key={`tile-${index}`} src={`/${path}`} alt="" />)}
-			</div>
+			</div>*/}
 		</header>
 		<main>
 			<BrowserRouter>
