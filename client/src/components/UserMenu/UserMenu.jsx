6633f09bea215e4e8be94196c7936064d83f713b
lisandroveron <lisandro.veron@hotmail.com>
2023-01-18 21:18:39 -0300

    Added: 'toggleMenu' function -> pressing the menu icon swipe the menu.

--- client/src/components/UserMenu/UserMenu.jsx
+++ client/src/components/UserMenu/UserMenu.jsx
@@ -1,4 +1,5 @@
 import {useSelector, useDispatch} from "react-redux";
+import "./UserMenu.css";
 
 export default function UserMenu(props){
 	const isLogged = useSelector(state => state.isLogged.value);
@@ -24,19 +25,38 @@ export default function UserMenu(props){
 				});
 			});
 	};
+	function toggleMenu(event){
+		const UserMenu_menu = document.getElementById("UserMenu_menu");
+		const UserMenu_icon = document.getElementById("UserMenu_icon");
+		const currentWidth = getComputedStyle(UserMenu_menu).width;
+		// If you change this, change "--username" CSS var too.
+		const maxMenuWidth = "200px";
+		console.log(getComputedStyle(UserMenu_icon));
 
-	return(<>
-	{
+		if(currentWidth === "0px"){
+			UserMenu_menu.style.setProperty("animation-name", "show");
+			UserMenu_icon.style.setProperty("position", "sticky");
+		}else if(currentWidth === maxMenuWidth){
+			UserMenu_menu.style.setProperty("animation-name", "hide");
+			UserMenu_icon.style.setProperty("position", "absolute");
+		};
+	};
+
+	return(
+	<div id="UserMenu">
+		<img id="UserMenu_icon" src="/static/assets/icon/project.svg" alt="" onClick={toggleMenu} />
+		<div id="UserMenu_menu">
+		{
 		isLogged
 		?<p>Estas loggeado</p>
 		:<form id="login" onSubmit={login}>
-			<input type="text" name="user" onChange={(event) => {
+			<input type="text" name="user" placeholder="Usuario" onChange={(event) => {
 				dispatch({
 					type: "username/set",
 					payload: event.target.value
 				});
 			}} />
-			<input type="password" name="password" onChange={(event) => {
+			<input type="password" name="password" placeholder="Contraseña" onChange={(event) => {
 				dispatch({
 					type: "password/set",
 					payload: event.target.value
@@ -44,6 +64,8 @@ export default function UserMenu(props){
 			}} />
 			<input type="submit" value="Iniciar sesión" />
 		</form>
-	}
-	</>);
+		}
+		</div>
+	</div>
+	);
 };
