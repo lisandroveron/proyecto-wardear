3f1d659975637987faa427d099f55529ac17fa95
lisandroveron <lisandro.veron@hotmail.com>
2023-01-23 19:55:28 -0300

    Added: First template for GuideMaker component with a <textarea> tag for writing guide. 'createGuide' function that sends the <textarea> content to the server.

--- client/src/router/GuideMaker/GuideMaker.jsx
+++ client/src/router/GuideMaker/GuideMaker.jsx
@@ -1,8 +1,38 @@
+import {useState} from "react";
 import {NavLink} from "react-router-dom";
+import {useSelector} from "react-redux";
 
 export default function GuideMaker(){
+	const isLogged = useSelector(state => state.isLogged.value);
+	const [content, setContent] = useState("");
+
+	function createGuide(event){
+		event.preventDefault();
+		fetch("/createguide", {
+			method: "POST",
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify({
+				content
+			})
+		})
+			.then(response => response.json())
+			.then(data => {
+				setContent(data.textarea);
+			});
+	};
+
 	return(<>
 		<NavLink to="/" className="router">←</NavLink>
 		<h2>Creador de guías</h2>
+		{
+			isLogged
+			? <>
+				<form onSubmit={createGuide}>
+					<textarea name="content" cols="30" rows="10" value={content} onChange={(event) => setContent(event.target.value)}></textarea>
+					<input type="submit" value="Guardar" />
+				</form>
+			</>
+			: <p>Tienes que iniciar sesión para crear una guía.</p>
+		}
 	</>)
 };
