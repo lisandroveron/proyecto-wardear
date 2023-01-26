54242290a08ea71b4c5c2f3ba872d8961fc4328d
lisandroveron <lisandro.veron@hotmail.com>
2023-01-26 17:25:37 -0300

    Modified: temporal title and description removed and chaged with props values.

--- client/src/components/GuideCard/GuideCard.jsx
+++ client/src/components/GuideCard/GuideCard.jsx
@@ -1,11 +1,11 @@
 import "./GuideCard.css";
 
-export default function GuideCard(){
+export default function GuideCard(props){
 	return(
 	<article className="GuideCard">
-		<h3>Lorem, ipsum dolor sit amet, consectetur adipisicing</h3>
+		<h3>{props.title}</h3>
 		<div></div>
-		<p>Lorem, ipsum dolor sit amet, consectetur adipisicing elit. Maxime vitae facilis tenetur blanditiis voluptatum, fugiat obcaecati reiciendis provident consequatur possimus incidunt alias doloribus, nulla quia repudiandae at officia quisquam totam.</p>
+		<p>{props.description}</p>
 	</article>
 	);
 };
