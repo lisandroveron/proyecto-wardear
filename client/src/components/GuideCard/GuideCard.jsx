7b5b9017a8009e0e2a6a7fc5ce94cdfbdd67837b
lisandroveron <lisandro.veron@hotmail.com>
2023-01-28 17:54:27 -0300

    Modified: <h2> changed to <NavLink> with props.

--- client/src/components/GuideCard/GuideCard.jsx
+++ client/src/components/GuideCard/GuideCard.jsx
@@ -1,9 +1,12 @@
+import {NavLink} from "react-router-dom";
 import "./GuideCard.css";
 
 export default function GuideCard(props){
 	return(
 	<article className="GuideCard">
-		<h3>{props.title}</h3>
+		<NavLink to="GuideReader" state={{id: props.id}}>
+			{props.title}
+		</NavLink>
 		<div></div>
 		<p>{props.description}</p>
 	</article>
